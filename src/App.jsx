31dac976d83d0c8502743c3279ace4d81f6e03c4
Disha Sharma <80575221+disha1917@users.noreply.github.com>
2023-05-07 19:33:04 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Disha Sharma",
+  title: "Machine Learning Enthusiast",
+  email: "dishaasharma1917@gmail.com",
+  gitHub: "disha1917",
+  instagram: "disha.1917",
+  linkedIn: "dishasharma1917",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "Disha_1917",
+  youTube: "LearnWithDisha14",
 };
 
 const primaryColor = "#4E567E";

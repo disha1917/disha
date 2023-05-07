ab68821657af633c7e96d7feafa18d6ea57e90e3
Disha Sharma <80575221+disha1917@users.noreply.github.com>
2023-05-07 19:43:58 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -23,19 +23,19 @@ import React from "react";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Software Engineer and Machine Learning enthusiast studying at Guru Nanak Dev University, Regional Campus, Jalandhar. I enjoy visuialiizing and predicting datasets in creative ways.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Data Science",
+  "Machine Learning",
+  "Graphic Designing",
+  "Java",
+  "Organizing and Managing",
+  "Youtube",   
 ];
 
 /**
@@ -44,7 +44,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my software engineering and Machine Learning experience, I continually look for new and better ways to make tech accessible by all.";
 
 const About = () => {
   return (
